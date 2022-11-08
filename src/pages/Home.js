8da27f3653ebb1402e4fed06116a57837406b785
abdulmalekFahd <abdulamlk819@gmail.com>
2022-11-08 15:34:54 +0300

    logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,7 @@
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import SearchSection from '../components/SearchSection';
-<<<<<<< HEAD
-// import svgPhoto from './svgPhoto';
-// import Subnavbar from './Subnavbar';
-import Category from '../pages/Category';
-// import { useNavigate } from 'react-router-dom';
-import Map from '../components/Map';
-=======
->>>>>>> 4e7719f036937678ff3f642dff989df4b64c6eaa
-
+import Map from '../components/Map'
 
 
 import '../styles/Home.css'
